51aabc3143ed116a49c1dd6a161d47a560399cac
Prakhar <prakharpalpfdr+github@gmail.com>
2021-06-23 00:54:33 +0530

    put components in a single file and render them using separate scripts'

--- components.js
+++ components.js
@@ -1,25 +1,38 @@
 'use strict';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
+//using IIFE to avoid polluting global scope
+(() => {
+  const { createElement: e, useState } = React;
+  class LikeButton extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { liked: false };
     }
 
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
+    render() {
+      if (this.state.liked) {
+        return 'You liked this.';
+      }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+      return e(
+        'button',
+        { onClick: () => this.setState({ liked: true }) },
+        'Like'
+      );
+    }
+  }
+
+  function Counter(){
+    const [count, setCount] = useState(0);
+    return (
+      <React.Fragment>
+        Counter is: {count}
+        <br/>
+        <button onClick={() => setCount(count+1)}>Increment Count</button>
+        {" "}
+        <button onClick={() => setCount(count-1)}>Decrement Count</button>
+      </React.Fragment>
+    )
+  }
+  window.LikeButton = LikeButton;
+  window.Counter = Counter;
+})()
